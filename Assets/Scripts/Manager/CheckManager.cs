57ac0a7cceacbb2b251fb0a77eb0bf2272d8e013
PengKunPro <1577011003@qq.com>
2019-06-19 20:12:09 +0800

    help

---
+++ Assets/Scripts/Manager/CheckManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class CheckManager  {
+    private static CheckManager _Instance=null;
+    public static CheckManager Instacne {
+        get {
+            if (_Instance == null) {
+                _Instance = new CheckManager();               
+            }
+            return _Instance;
+        }
+    } 
+    
+}
---
+++ Assets/Scripts/UI/Panel/CheckPanel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPanel : IView {
+    protected override void OnDestroy()
+    {
+       
+    }
+    protected override void OnHide()
+    {
+        
+    }       
+    protected override void OnStart()
+    {
+
+    }
+    protected override void OnShow()
+    {
+
+    }
+    public override void Update()
+    {
+       
+    }
+
+}
